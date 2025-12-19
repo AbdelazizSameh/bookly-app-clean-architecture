a8fc4ef7023fe6c41571913e83eb3ec59a3bf7e8
AbdelazizSameh <abdelzizsameh364@gmail.com>
2025-12-19 17:15:20 +0200

    create fetchFeaturedBooks UseCase

---
+++ lib/Features/home/domain/use_cases/fetch_featured_books_use_case.dart
@@ -0,0 +1,14 @@
+import 'package:dartz/dartz.dart';
+
+import '../../../../core/errors/failure.dart';
+import '../entities/book_entity.dart';
+import '../repos/home_repo.dart';
+
+class FetchFeaturedBooksUseCase {
+  final HomeRepo homeRepo;
+
+  FetchFeaturedBooksUseCase({required this.homeRepo});
+  Future<Either<Failure, List<BookEntity>>> fetchFeaturedBooks() {
+    return homeRepo.fetchFeaturedBooks();
+  }
+}
