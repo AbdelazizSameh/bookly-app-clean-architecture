0e05b720875dead0e4e3a45c585a47f455eb672b
AbdelazizSameh <abdelzizsameh364@gmail.com>
2025-12-19 16:59:00 +0200

    add home repo and Failure calss

---
+++ lib/Features/home/domain/repos/home_repo.dart
@@ -0,0 +1,7 @@
+import '../../../../core/errors/failure.dart';
+import '/Features/home/domain/entities/book_entity.dart';
+import 'package:dartz/dartz.dart';
+
+abstract class HomeRepo {
+  Future<Either<Failure, List<BookEntity>>> fetchFeaturedBooks();
+}
