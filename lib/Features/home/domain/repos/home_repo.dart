551ae194861a5a54061644a43d1f729542ab1d2e
AbdelazizSameh <abdelzizsameh364@gmail.com>
2025-12-19 19:28:13 +0200

    create FetchNewestBooksUseCase

--- lib/Features/home/domain/repos/home_repo.dart
+++ lib/Features/home/domain/repos/home_repo.dart
@@ -4,4 +4,5 @@ import 'package:dartz/dartz.dart';
 
 abstract class HomeRepo {
   Future<Either<Failure, List<BookEntity>>> fetchFeaturedBooks();
+  Future<Either<Failure, List<BookEntity>>> fetchNewestBooks();
 }
