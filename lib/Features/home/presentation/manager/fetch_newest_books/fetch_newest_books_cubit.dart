2c9d1ab83f5da28759e8e1e9c8e08dff980e3963
AbdelazizSameh <abdelzizsameh364@gmail.com>
2026-02-05 23:37:09 +0200

    implement fetch newest books cubit, states and function

--- lib/Features/home/presentation/manager/fetch_newest_books/fetch_newest_books_cubit.dart
+++ lib/Features/home/presentation/manager/fetch_newest_books/fetch_newest_books_cubit.dart
@@ -1,8 +1,26 @@
 import 'package:bloc/bloc.dart';
+import 'package:bookly_app/Features/home/domain/entities/book_entity.dart';
+import 'package:bookly_app/Features/home/domain/use_cases/fetch_newest_books_use_case.dart';
 import 'package:meta/meta.dart';
 
 part 'fetch_newest_books_state.dart';
 
 class FetchNewestBooksCubit extends Cubit<FetchNewestBooksState> {
-  FetchNewestBooksCubit() : super(FetchNewestBooksInitial());
+  FetchNewestBooksCubit(this.fetchNewestBooksUseCase)
+    : super(FetchNewestBooksInitialState());
+
+  final FetchNewestBooksUseCase fetchNewestBooksUseCase;
+
+  Future<void> fetchFeaturedBooks() async {
+    emit(FetchNewestBooksInitialState());
+    var result = await fetchNewestBooksUseCase.call();
+    result.fold(
+      (failure) {
+        emit(FetchNewestBooksFailureState(errorMessage: failure.errorMessage));
+      },
+      (books) {
+        emit(FetchNewestBooksSuccessState(books: books));
+      },
+    );
+  }
 }
--- lib/Features/home/presentation/manager/fetch_newest_books/fetch_newest_books_state.dart
+++ lib/Features/home/presentation/manager/fetch_newest_books/fetch_newest_books_state.dart
@@ -3,4 +3,18 @@ part of 'fetch_newest_books_cubit.dart';
 @immutable
 sealed class FetchNewestBooksState {}
 
-final class FetchNewestBooksInitial extends FetchNewestBooksState {}
+final class FetchNewestBooksInitialState extends FetchNewestBooksState {}
+
+final class FetchNewestBooksLoadingState extends FetchNewestBooksState {}
+
+final class FetchNewestBooksFailureState extends FetchNewestBooksState {
+  final String errorMessage;
+
+  FetchNewestBooksFailureState({required this.errorMessage});
+}
+
+final class FetchNewestBooksSuccessState extends FetchNewestBooksState {
+  final List<BookEntity> books;
+
+  FetchNewestBooksSuccessState({required this.books});
+}
