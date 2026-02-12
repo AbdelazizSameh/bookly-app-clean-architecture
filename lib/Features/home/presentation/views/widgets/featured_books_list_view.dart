4f6e5a1db714e2623c662d4e9318cca8164d4027
AbdelazizSameh <abdelzizsameh364@gmail.com>
2026-02-12 18:10:34 +0200

    convert the widget to stateful widget and use scroll controller

--- lib/Features/home/presentation/views/widgets/featured_books_list_view.dart
+++ lib/Features/home/presentation/views/widgets/featured_books_list_view.dart
@@ -1,20 +1,53 @@
 import 'package:bookly_app/Features/home/domain/entities/book_entity.dart';
+import 'package:bookly_app/Features/home/presentation/manager/featured_books_cubit/featured_books_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'custom_book_item.dart';
 
-class FeaturedBooksListView extends StatelessWidget {
+class FeaturedBooksListView extends StatefulWidget {
   const FeaturedBooksListView({super.key, required this.books});
   final List<BookEntity> books;
+
+  @override
+  State<FeaturedBooksListView> createState() => _FeaturedBooksListViewState();
+}
+
+class _FeaturedBooksListViewState extends State<FeaturedBooksListView> {
+  late final ScrollController _scrollController;
+
+  @override
+  void initState() {
+    _scrollController = ScrollController();
+    _scrollController.addListener(_viewMoreResults);
+    super.initState();
+  }
+
+  void _viewMoreResults() {
+    var currentPosition = _scrollController.position.pixels,
+        maxLength = _scrollController.position.maxScrollExtent;
+
+    if (currentPosition >= 0.7 * maxLength) {
+      BlocProvider.of<FeaturedBooksCubit>(context).fetchFeaturedBooks();
+    }
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       height: MediaQuery.sizeOf(context).height * 0.3,
       child: ListView.separated(
+        controller: _scrollController,
         padding: EdgeInsets.symmetric(horizontal: 12),
         scrollDirection: Axis.horizontal,
-        itemCount: books.length,
+        itemCount: widget.books.length,
         itemBuilder: (context, index) =>
-            CustomBookItem(bookEntity: books[index]),
+            CustomBookItem(bookEntity: widget.books[index]),
         separatorBuilder: (BuildContext context, int index) =>
             const SizedBox(width: 12),
       ),
